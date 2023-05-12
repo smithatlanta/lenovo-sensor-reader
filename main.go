4978cbbbf2bbeb7d44351cddf96da4b8e5a9c5cb
Michael Smith <smithatlanta@gmail.com>
2023-05-12 16:44:04 -0400

    a little slow

--- main.go
+++ main.go
@@ -29,10 +29,10 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("influxUrl:", influxUrl)
-	fmt.Println("influxAuth:", influxAuth)
-	fmt.Println("influxTable:", influxTable)
-	fmt.Println("computerName:", computerName)
+	fmt.Println("influxUrl:", *influxUrl)
+	fmt.Println("influxAuth:", *influxAuth)
+	fmt.Println("influxTable:", *influxTable)
+	fmt.Println("computerName:", *computerName)
 
 	client := influxdb2.NewClient(*influxUrl, *influxAuth)
 	writeAPI := client.WriteAPIBlocking("", *influxTable)
